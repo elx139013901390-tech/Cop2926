9c3bb4d07fbae15d9e6dcca393eba67ccda39e5a
elx139013901390-tech <elx139013901390@gmail.com>
2026-06-23 04:59:33 +0330

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,30 +1,26 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import {
-getAuth
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import {
-getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-/*
-اطلاعات Firebase خودت را اینجا قرار بده
-*/
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
 
-apiKey: "YOUR_API_KEY",
+apiKey: "AIzaSyDSEmYT1lPfLCzRLyvS9i7IVm1ULbvkONg",
 
-authDomain: "YOUR_PROJECT.firebaseapp.com",
+authDomain: "cop20206iran.firebaseapp.com",
 
-projectId: "YOUR_PROJECT_ID",
+projectId: "cop20206iran",
 
-storageBucket: "YOUR_PROJECT.appspot.com",
+storageBucket: "cop20206iran.firebasestorage.app",
 
-messagingSenderId: "YOUR_SENDER_ID",
+messagingSenderId: "145250732658",
 
-appId: "YOUR_APP_ID"
+appId: "1:145250732658:web:600b14952a3b32f15d90eb",
+
+measurementId: "G-QYT1PENFJJ"
 
 };
 
