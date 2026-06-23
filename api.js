c6f9bedacd16763196bf26d8285728cdb6890b50
elx139013901390-tech <elx139013901390@gmail.com>
2026-06-23 05:06:14 +0330

    Update api.js

--- api.js
+++ api.js
@@ -1,14 +1,12 @@
-const API_KEY = "d5607c2a5b90d9ae38708b37a1610be0";
+const API_KEY = "YOUR_API_KEY";
 
 export async function getMatches(){
-
-try{
 
 const response = await fetch(
 "https://v3.football.api-sports.io/fixtures?next=20",
 {
 headers:{
-"x-apisports-key":API_KEY
+"x-apisports-key": API_KEY
 }
 }
 );
@@ -16,13 +14,5 @@ headers:{
 const data = await response.json();
 
 return data.response;
-
-}catch(error){
-
-console.log(error);
-
-return [];
-
-}
 
 }
