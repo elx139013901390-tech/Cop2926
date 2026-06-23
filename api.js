3262b487b4fc6321e4ba3794955ded8169fe4f15
elx139013901390-tech <elx139013901390@gmail.com>
2026-06-23 05:07:01 +0330

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
-const API_KEY = "YOUR_API_KEY";
+import { getMatches } from "./api.js";
+const API_KEY = "d5607c2a5b90d9ae38708b37a1610be0";
 
 export async function getMatches(){
 
@@ -14,5 +15,51 @@ headers:{
 const data = await response.json();
 
 return data.response;
+
+}
+async function loadMatches(){
+
+const matchesDiv =
+document.getElementById("matches");
+
+matchesDiv.innerHTML = "در حال بارگذاری...";
+
+try{
+
+const matches =
+await getMatches();
+
+matchesDiv.innerHTML = "";
+
+matches.forEach(match=>{
+
+matchesDiv.innerHTML += `
+
+<div style="
+padding:10px;
+margin:10px 0;
+background:#1F2937;
+border-radius:10px;
+">⚽ ${match.teams.home.name}
+
+vs
+
+${match.teams.away.name}
+
+<br><br>
+
+🕒 ${new Date(
+match.fixture.date
+).toLocaleString("fa-IR")}
+
+</div>
+`;});
+
+}catch(error){
+
+matchesDiv.innerHTML =
+"خطا در دریافت مسابقات";
+
+}
 
 }
